1d489992807aa46c1adbba89749f95570a2a8002
tranthaibao29112001 <75375136+tranthaibao29112001@users.noreply.github.com>
2022-01-16 21:47:11 +0700

    Re commit Patient

--- routes/patient.js
+++ routes/patient.js
@@ -4,11 +4,14 @@ const route = express.Router();
 
 const patientController = require('../controllers/PatientControler');
 
-route.get('/detailInfo', patientController.detailInfo);
+route.get('/detailInfo/:patientID', patientController.detailInfo);
 route.get('/payment', patientController.payment);
 route.get('/manageHistory', patientController.manageHistory);
-route.get('/supplies', patientController.supplies);
+route.get('/packages', patientController.packages);
+route.get('/packages/:packageID',patientController.packageDetail);
 route.get('/changePassword', patientController.changePassword);
 route.get('/',patientController.home);
+
+route.post('/buyPacket/:packageID', patientController.buyPackage);
 
 module.exports = route;
